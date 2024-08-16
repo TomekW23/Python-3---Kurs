5d7494725e43931a097890631ad910d8f2dc5852
Tomasz Wieczorek <83585@student.ue.poznan.pl>
2024-08-16 18:21:55 +0200

    Dodanie plików z pętlami for i while

---
+++ for.py
@@ -0,0 +1,10 @@
+wynik = 0
+
+
+i = 0
+for i in range(0, 4):
+    x = int(input("Podaj liczbę: "))
+    wynik += x
+
+print("Wynik dodawania liczb to: ", wynik)
+
---
+++ while.py
@@ -0,0 +1,14 @@
+#PĘTLA
+
+
+#suma = 0
+# x = int(input("Podaj kolejną liczbę: "))
+
+
+liczba = 100
+
+while liczba >= 0:
+    print(liczba)
+    liczba -= 1
+
+
