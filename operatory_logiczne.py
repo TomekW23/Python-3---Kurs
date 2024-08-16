93191e33e9d1b50c456d12a42770373fccac5411
Tomasz Wieczorek <83585@student.ue.poznan.pl>
2024-08-16 17:59:35 +0200

    Dodanie pliku z operatorów logicznych

---
+++ operatory_logiczne.py
@@ -0,0 +1,36 @@
+wartosc = int(input("Sprawdzę, czy liczba jest z zakresu od 1 do 10: "))
+
+if(wartosc > 1 and wartosc < 10):
+    print("Wartość jest od 1 do 10")
+
+a = 5
+b = 2
+if(not(a == 5 or b ==3)):
+    print("tak")
+
+
+'''
+OPERATORY LOGICZNE
+
+and - 'i'
+
+True True - True
+True False - False
+False True - False
+False False - False
+
+Komunikacja jest prawdziwa, wtedy i tylko wtedy, gdy OBA wyrażenia są prawidłowe
+
+or - 'lub'
+
+True True - True
+True False - True
+false True - True
+False False - False
+
+Alternatywa jest fałszywa wtedy i tylko wtedy, gdy obba wyrażenia w tym samym momencie są fałszywe
+
+not - 'nie'
+
+
+'''
